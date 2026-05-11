4a7858e039632383122d1d60d0f7fdea9f73c20c
swatijadhavtech <swatijadhav3march@gmail.com>
2026-05-11 07:09:47 +0530

    Task on operator, variable and string

---
+++ Task/T2/Basic_Code.py
@@ -0,0 +1,7 @@
+print(3 + 2)   # addition(+)
+print(3 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
